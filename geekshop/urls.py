fd7840e55538d7fe73d0b9313abbe77b1c473569
AlekseyB86 <avbelov86@mail.ru>
2021-11-23 23:19:30 +0300

    добавил базовый шаблон base.html, подшаблоны inc_categories_menu.html и inc_main_menu.html

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,8 +18,14 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.main, name='index'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
+
+    path('products/products_home/', mainapp.products_home, name='products_home'),
+    path('products/products_office/', mainapp.products_office, name='products_office'),
+    path('products/products_modern/', mainapp.products_modern, name='products_modern'),
+    path('products/products_classic/', mainapp.products_classic, name='products_classic'),
+
     path('admin/', admin.site.urls),
 ]
