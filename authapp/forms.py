38faa9fbab59a0cacee202bd16370584b46e3429
AlekseyB86 <avbelov86@mail.ru>
2022-01-10 21:09:46 +0300

    part2_les2 - OnetoOne ShopUser - userprofile - edit userprofile

--- authapp/forms.py
+++ authapp/forms.py
@@ -2,7 +2,7 @@ import hashlib, random
 
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
-from authapp.models import ShopUser
+from authapp.models import ShopUser, ShopUserProfile
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -56,3 +56,24 @@ class ShopUserEditForm(UserChangeForm):
             field.help_text = ''
             if field_name == 'password':
                 field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды")
+        return data
+
+
+class ShopUserProfileEdit(forms.ModelForm):
+
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'about_me', 'gender',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
