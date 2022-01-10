309e44b62377f5d7b1bba2ee336b399cd11cc0d7
AlekseyB86 <avbelov86@mail.ru>
2022-01-10 20:25:23 +0300

    part2_les2 - migrate ShopUserProfile - update_db

--- authapp/models.py
+++ authapp/models.py
@@ -4,6 +4,8 @@ import pytz
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 NULLABLE = {'null': True, 'blank': True}
 
@@ -21,3 +23,37 @@ class ShopUser(AbstractUser):
     def delete(self, using=None, keep_parents=False):
         self.is_active = not self.is_active
         self.save()
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(ShopUser, on_delete=models.CASCADE, unique=True, db_index=True)
+    tagline = models.CharField(max_length=150, blank=True, verbose_name='тэги')
+    about_me = models.TextField(blank=True, verbose_name='о себе')
+    gender = models.CharField(choices=GENDER_CHOICES, blank=True, max_length=1, verbose_name='пол')
+
+    # В db лежит
+    # user_profile.gender
+    # W
+    # в шаблонах в отображении
+    # user_profile.get_gender_display()
+    # user_profile.get_gender_display
+    # Ж
+
+#     Сигналы
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
+
