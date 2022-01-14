f6950fecbb738f2b44641747aa582d408dff6827
eghiq <tigrxam@gmail.com>
2022-01-14 20:47:43 +0300

    part2_les3 - ordersapp: update

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -31,12 +31,44 @@ class OrderItemCreate(CreateView):
             basket_items = Basket.objects.filter(user=self.request.user)
             if basket_items.exists():
                 OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=basket_items.count())
-            formset = OrderFormset()
-            for num, form in enumerate(formset.forms):
-                form.initial['product'] = basket_items[num].product
-                form.initial['quantity'] = basket_items[num].quantity
+                formset = OrderFormset()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+                # basket_items.delete()
             else:
                 formset = OrderFormset()
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            Basket.objects.filter(user=self.request.user).delete()
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        return super().form_valid(form)
+
+
+class OrderItemUpdate(UpdateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:order_list')
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.method == 'POST':
+            formset = OrderFormset(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormset(instance=self.object)
         data['orderitems'] = formset
         return data
 
@@ -52,10 +84,6 @@ class OrderItemCreate(CreateView):
                 orderitems.save()
 
         return super().form_valid(form)
-
-
-class OrderItemUpdate(UpdateView):
-    pass
 
 
 class OrderItemDetail(DetailView):
