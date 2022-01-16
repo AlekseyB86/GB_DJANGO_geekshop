3058209a896977c0920a6cbbb748bd8372592635
pe4ka <pe4kakamni@gmail.com>
2022-01-16 12:24:41 +0300

    part2_les4 - jquery.formset.js

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -55,4 +55,19 @@ window.onload = function () {
         $('.order_total_cost').html(order_total_cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ").replace('.', ','));
     }
 
+
+    $('.formset_row').formset({
+        addText: "добавить товар",
+        deleteText: "удалить",
+        prefix: "orderitems",
+        removed: deleteOrderItem
+    });
+
+    function deleteOrderItem(row){
+        var target_name = row[0].querySelector('input[type="number"]').name;
+        orderitem_num = target_name.replace('orderitems-', '').replace('-quantity', '');
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
+
 };
